3fb6a63ed118ce5daf26c107407c88e2b83c029c
moaz khaled <mo3az.khalid.2003@gmail.com>
2025-01-18 00:04:17 +0200

    Add files via upload

---
+++ Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/Summation_from_1_to_N.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int Num;
+    cin >> Num;
+    long long int result = Num  * (Num + 1) / 2;
+    cout << result;
+    return 0;
+}
