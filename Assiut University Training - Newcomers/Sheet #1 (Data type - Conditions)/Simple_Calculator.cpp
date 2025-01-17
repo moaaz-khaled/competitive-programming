2469a0860155104fdb6a515489364b055498232b
moaz khaled <mo3az.khalid.2003@gmail.com>
2025-01-18 00:00:55 +0200

    Add files via upload

---
+++ Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/Simple_Calculator.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long int N1,N2;
+    cin >> N1 >> N2;
+    cout << N1 << " + " << N2 << " = " << N1 + N2 << endl;
+    cout << N1 << " * " << N2 << " = " << N1 * N2 << endl;
+    cout << N1 << " - " << N2 << " = " << N1 - N2 << endl;
+    return 0;
+}
