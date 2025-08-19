04ba371f95aa93fd28eb7b423a7af4ad44029276
moaaz-khaled <mo3az.khalid.2003@gmail.com>
2025-08-19 03:26:17 +0300

    Solve Problem I J

---
+++ Assiut University Training - Newcomers/Data Type/Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define pi 3.141592653
+
+
+int main(void)
+{   
+    fast;
+    int A , B;
+    cin >> A >> B;
+    if(A >= B)
+        cout << "Yes";
+    else
+        cout << "No";
+    return 0;
+}
